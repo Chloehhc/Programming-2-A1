f0fd50e9dac50a58bd6069536e26cbaefcab868e
Chloe Hsieh <chloehhc@gmail.com>
2021-01-19 11:18:20 -0800

    commit

--- pygame/shmup.py
+++ pygame/shmup.py
@@ -1,9 +1,9 @@
 # shmup.py
 # Top-down shoot-m-up game
 # Goals:
-#   - get more comfortable with sprites and mouse
-#   - create new sprites IN the main loop
-#   - using pygame.mouse a little more comfortably
+#    - get more comfortable with sprites and mouse
+#    - create new sprites IN the main loop (bullets)
+#    - using pygame.mouse a little more comfortably
 
 import pygame
 
@@ -12,10 +12,71 @@ BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 YELLOW = (255, 255, 0)
 SKY_BLUE = (95, 165, 228)
-WIDTH = 800
-HEIGHT = 600
-TITLE = "<You're title here>"
+WIDTH = 720
+HEIGHT = 1280
+TITLE = "SHMUP"
 
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+
+        self.image = pygame.image.load("./images/galaga_ship.png")
+        # scaling the image down 0.5x
+        self.image = pygame.transform.scale(self.image, (64, 64))
+
+        self.rect = self.image.get_rect()
+
+    def update(self):
+        """Move the player with the mouse"""
+        self.rect.center = pygame.mouse.get_pos()
+
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self, y_coord):
+        """
+        Arguments:
+            y_coord - initial y-coordinate
+        """
+        super().__init__()
+
+        self.image = pygame.image.load("./images/mario.png")
+
+        self.rect = self.image.get_rect()
+
+        # initial location middle of the screen at y_coord
+        self.rect.centerx = WIDTH / 2
+        self.rect.centery = y_coord
+
+        self.x_vel = 3
+
+    def update(self):
+        """Move the enemy side-to-side"""
+        self.rect.x += self.x_vel
+
+        # Keep enemy in the screen
+        if self.rect.right > WIDTH or self.rect.left < 0:
+            self.x_vel *= -1
+
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self, coords):
+        """
+
+        Arguments:
+            coords - tuple of x,y
+        """
+
+        super().__init__()
+        self.image = pygame.image.load("./images/bullet.png")
+        # scale to 22x36px
+        self.image = pygame.transform.scale(self.image, (22, 36))
+
+        self.rect = self.image.get_rect()
+        # Start the bullet at coords
+        self.rect.centerx, self.rect.y = coords
+
+        self.y_vel = -3
+
+    def update(self):
+        self.rect.y += self.y_vel
 
 def main():
     pygame.init()
@@ -29,20 +90,54 @@ def main():
     done = False
     clock = pygame.time.Clock()
 
+    # Sprite Groups
+    all_sprites = pygame.sprite.RenderUpdates()
+    enemy_sprites = pygame.sprite.Group()
+    player_bullet_sprites = pygame.sprite.Group()
+
+    #  --- enemies
+    enemy = Enemy(100)
+    all_sprites.add(enemy)
+    enemy_sprites.add(enemy)
+
+    #  --- player
+    player = Player()
+    all_sprites.add(player)
+
     # ----- MAIN LOOP
     while not done:
         # -- Event Handler
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 done = True
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                # create a bullet
+                bullet = Bullet(player.rect.midtop)
+                all_sprites.add(bullet)
+                player_bullet_sprites.add(bullet)
 
         # ----- LOGIC
+        all_sprites.update()
+
+        # check if every bullet has collided with enemy
+        for bullet in player_bullet_sprites:
+            # Kill it off screen
+            if bullet.rect.bottom < 0:
+                bullet.kill()
+
+            # Enemy collision
+            enemies_hit_group = pygame.sprite.spritecollide(bullet, enemy_sprites, True)
+            if len(enemies_hit_group) > 0:
+                bullet.kill()
+
 
         # ----- DRAW
         screen.fill(BLACK)
+        dirty_rectangles = all_sprites.draw(screen)
 
         # ----- UPDATE
-        pygame.display.flip()
+        # update only dirty rectangles
+        pygame.display.update(dirty_rectangles)
         clock.tick(60)
 
     pygame.quit()
